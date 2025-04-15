aa1919b40a986dd91ddb62a75b2238d681c3c68f
Mohamed-GT3 <talaatmohamed145@gmail.com>
2025-04-15 21:12:28 +0200

    Added comment to Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import { Spotlight } from './ui/spotlight'
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
+      {/* Comment */}
       <div>
         <Spotlight className="-top-40 -left-10
         md:-left-32 md:-top-20 h-screen" fill="white" />
